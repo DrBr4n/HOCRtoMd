391e0cb9380db3f9ea4523b527c884b542ed8b5d
DrBr4n <brunocunhasilva2001@gmail.com>
2023-03-16 18:44:49 +0000

    help() and choose psm mode

--- parsehocr.py
+++ parsehocr.py
@@ -3,12 +3,33 @@ from PIL import Image, ImageDraw
 import sys 
 import pytesseract
 
+def help():
+    print("""
+Page segmentation modes:
+  0    Orientation and script detection (OSD) only.
+  1    Automatic page segmentation with OSD.
+  2    Automatic page segmentation, but no OSD, or OCR. (not implemented)
+  3    Fully automatic page segmentation, but no OSD. (Default)
+  4    Assume a single column of text of variable sizes.
+  5    Assume a single uniform block of vertically aligned text.
+  6    Assume a single uniform block of text.
+  7    Treat the image as a single text line.
+  8    Treat the image as a single word.
+  9    Treat the image as a single word in a circle.
+ 10    Treat the image as a single character.
+ 11    Sparse text. Find as much text as possible in no particular order.
+ 12    Sparse text with OSD.
+ 13    Raw line. Treat the image as a single text line,
+       bypassing hacks that are Tesseract-specific.
+
+usage: python3 parsehocr.py pathToFile pathToOut """)
+
 def parseArgv():
     # open image from argv[1]
     image = Image.open(sys.argv[1])
 
     # Create HOCR output in argv[2]
-    hocr = pytesseract.image_to_pdf_or_hocr(image, lang="por", extension='hocr')
+    hocr = pytesseract.image_to_pdf_or_hocr(image, config='--psm ' + sys.argv[3] , lang="por", extension='hocr')
     with open(sys.argv[2], 'w+b') as f:
         f.write(hocr) 
     
@@ -86,6 +107,10 @@ def drawBoxes(image, data, photos):
     image.show()
 
 def main():
+
+    if (len(sys.argv) == 1) : 
+        help()
+        return 
     image = parseArgv()
     data = parseHocr()
     photos = extractPhotos()
