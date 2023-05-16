70e99c71f9ed41b87146deaddbce769bf3ec4ed0
Bruno Silva <brunocunhasilva2001@gmail.com>
2023-05-16 02:02:03 +0100

    added help menu and cli-like capabilities, crude organizeArticles

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-from PIL import Image, ImageDraw
+import PIL.Image
+import PIL.ImageDraw
 import xml.etree.ElementTree as ET
 import pytesseract
 import copy
@@ -10,16 +11,16 @@ class Page:
         self.id = id
         self.careas = []
         self.careasIdx = -1
-        self.photos = []
-        self.photosIdx = -1
+        self.images = []
+        self.imagesIdx = -1
 
     def addCarea(self, carea):
         self.careasIdx += 1
         self.careas.append(carea)
 
-    def addPhoto(self, photo):
-        self.photosIdx += 1
-        self.photos.append(photo)
+    def addImage(self, sourceImage):
+        self.imagesIdx += 1
+        self.images.append(sourceImage)
 
     def addPar(self, par): self.careas[self.careasIdx].addPar(par)
     def addLine(self, line): self.careas[self.careasIdx].addLine(line)
@@ -77,25 +78,10 @@ class Word:
         self.text = text
 
 
-class Photo:
+class My_Image:
     def __init__(self, id, bbox):
         self.id = id
         self.bbox = bbox
-
-    def getId(self): return self.id
-    def getBbox(self): return self.bbox
-
-
-def parseArgv():
-    # open image from argv[1]
-    image = Image.open(sys.argv[1])
-
-    # Create HOCR output in argv[2]
-    hocr = pytesseract.image_to_pdf_or_hocr(image, config='--psm ' + sys.argv[3], lang="por", extension='hocr')
-    with open(sys.argv[2], 'w+b') as f:
-        f.write(hocr)
-
-    return image
 
 
 def parseHocr():
@@ -132,93 +118,111 @@ def parseHocr():
             case 'ocrx_word':
                 p1.addWord(Word(child.get('id'), bbox, x_wconf, child.text))
             case 'ocr_photo':
-                p1.addPhoto(Photo(child.get('id'), bbox))
+                p1.addImage(My_Image(child.get('id'), bbox))
 
     return p1
 
 
-def drawCareaBoxes(image, pageObject):
+def drawCareaBoxes(sourceImage, pageObject):
     for carea in pageObject.careas:
-        x, y, x2, y2 = carea.bbox[0] ,carea.bbox[1], carea.bbox[2], carea.bbox[3]
-        ImageDraw.Draw(image).rectangle([x, y, x2, y2], fill=None, outline='red', width=3)
+        x, y = carea.bbox[0], carea.bbox[1]
+        x2, y2 = carea.bbox[2], carea.bbox[3]
+        PIL.ImageDraw.Draw(sourceImage).rectangle([x, y, x2, y2],
+                                                  fill=None,
+                                                  outline='red',
+                                                  width=3)
 
 
-def drawParBoxes(image, pageObject):
+def drawParBoxes(sourceImage, pageObject):
     for carea in pageObject.careas:
         for paragraph in carea.pars:
-            x, y, x2, y2 = paragraph.bbox[0], paragraph.bbox[1], paragraph.bbox[2], paragraph.bbox[3]
-            ImageDraw.Draw(image).rectangle([x, y, x2, y2], fill=None, outline='blue', width=2)
+            x, y = paragraph.bbox[0], paragraph.bbox[1]
+            x2, y2 = paragraph.bbox[2], paragraph.bbox[3]
+            PIL.ImageDraw.Draw(sourceImage).rectangle([x, y, x2, y2],
+                                                      fill=None,
+                                                      outline='blue',
+                                                      width=2)
 
 
-def drawLinesBoxes(image, pageObject):
+def drawLinesBoxes(sourceImage, pageObject):
     for carea in pageObject.careas:
         for paragraph in carea.pars:
             for line in paragraph.lines:
-                x, y, x2, y2 = line.bbox[0], line.bbox[1], line.bbox[2], line.bbox[3]
-                ImageDraw.Draw(image).rectangle([x, y, x2, y2], fill=None, outline='green')
+                x, y = line.bbox[0], line.bbox[1]
+                x2, y2 = line.bbox[2], line.bbox[3]
+                PIL.ImageDraw.Draw(sourceImage).rectangle([x, y, x2, y2],
+                                                          fill=None,
+                                                          outline='green')
 
 
-def drawPhotosBoxes(image, pageObject):
-    for photo in pageObject.photos:
-        x, y, x2, y2 = photo.bbox[0], photo.bbox[1], photo.bbox[2], photo.bbox[3]
-        ImageDraw.Draw(image).rectangle([x, y, x2, y2], fill=None, outline='purple')
+def drawImageBoxes(sourceImage, pageObject):
+    for image in pageObject.images:
+        x, y = image.bbox[0], image.bbox[1],
+        x2, y2 = image.bbox[2], image.bbox[3]
+        PIL.ImageDraw.Draw(sourceImage).rectangle([x, y, x2, y2],
+                                                  fill=None,
+                                                  outline='purple')
 
 
-def drawArticlesBoxes(image, artigos):
+def drawArticlesBoxes(sourceImage, artigos):
     for artigo in artigos:
-        x, y, x2, y2 = artigo.bbox[0], artigo.bbox[1], artigo.bbox[2], artigo.bbox[3]
-        ImageDraw.Draw(image).rectangle([x, y, x2, y2], fill=None, outline='blue', width=2)
+        x, y = artigo.bbox[0], artigo.bbox[1]
+        x2, y2 = artigo.bbox[2], artigo.bbox[3]
+        PIL.ImageDraw.Draw(sourceImage).rectangle([x, y, x2, y2],
+                                                  fill=None,
+                                                  outline='blue',
+                                                  width=2)
 
 
-def extractPhotos(image, pageObject):
-    for photo in pageObject.photos:
-        x, y, x2, y2 = photo.bbox[0], photo.bbox[1], photo.bbox[2], photo.bbox[3]
-        tmpImage = image.crop((x, y, x2, y2))
-        tmpImage.save("out/" + photo.id + ".jpg")
-
-    for carea in pageObject.careas:
-        print(f"Carea: id:{carea.id} bbox:{carea.bbox}")
-        for par in carea.pars:
-            print(f"\tParagraph: id:{par.id} bbox:{par.bbox} lang:{par.lang}")
-            for line in par.lines:
-                print(f"\t\tLine: id:{line.id} bbox:{line.bbox} x_size:{line.x_size}")
-                print(f"\t\t\tWords: {' '.join([word.text for word in line.words])}")
-
-
-# Carea: id, bbox, pars[Par], parsIdx
-# Par: id, bbox, lang, lines[Line], linesIdx
-# Line: id, bbox, x_size, words[Word], wordIdx
-# Word: id, bbox, x_wconf, text
-
-# articles is a list of careas
-def createArticles(pageObject):
-    articles = copy.deepcopy(pageObject.careas)
-    return articles
+def extractImages(sourceImage, pageObject):
+    for image in pageObject.images:
+        x, y = image.bbox[0], image.bbox[1]
+        x2, y2 = image.bbox[2], image.bbox[3]
+        tmpImage = sourceImage.crop((x, y, x2, y2))
+        tmpImage.save("out/" + image.id + ".jpg")
 
 
 def organizeArticles(articles):
+
+    nAr = len(articles) - 1
+    idx = 0
+    lastIdx = 0
+    while (idx < nAr):
+        if articles[idx].pars[0].lines[0].x_size > articles[idx+1].pars[0].lines[0].x_size:
+            # if articles[idx+1].bbox[3] - articles[idx].bbox[3] < 100:
+            articles[idx].pars += articles[idx+1].pars
+            del articles[idx+1]
+            nAr -= 1
+        else:
+            idx += 1
+            lastIdx += 1
+
+
     toRemove = []
+    """
     for idx in range(len(articles) - 1):
         # if x_size1 > x_size2
-        print(articles[idx].pars[0].lines[0].x_size ,articles[idx+1].pars[0].lines[0].x_size)
+        print(articles[idx].id, articles[idx].pars[0].lines[0].x_size, articles[idx].id, articles[idx+1].pars[0].lines[0].x_size)
         if articles[idx].pars[0].lines[0].x_size > articles[idx+1].pars[0].lines[0].x_size:
             print('toRemove')
             articles[idx].pars += articles[idx+1].pars
             toRemove.append(articles[idx+1].id)
+            del articles[idx+1]
+    """
         # if bbox1 is shorter than bbox2
-        #if articles[idx].bbox[2] < articles[idx+1].bbox[2]:
+        # if articles[idx].bbox[2] < articles[idx+1].bbox[2]:
         #    articles[idx].pars += articles[idx+1].pars
         #    toRemove.append(articles[idx+1].id)
 
-    for article in articles:
-        if article.id in toRemove:
-            articles.remove(article)
+    #for article in articles:
+    #    if article.id in toRemove:
+    #        articles.remove(article)
 
     return articles
 
 
 def createMarkdown(articles):
-    f = open("out/out.txt", 'w')
+    f = open("out/out.md", 'w')
     for article in articles:
         f.write("\n\n___\n\n")
         for par in article.pars:
@@ -227,23 +231,74 @@ def createMarkdown(articles):
             f.write("\n")
 
 
-def main():
-    image = parseArgv()
-    page1 = parseHocr()
+def help():
+    print("""
+Page segmentation modes:
+      0    Orientation and script detection (OSD) only.
+      1    Automatic page segmentation with OSD.
+      2    Automatic page segmentation, but no OSD, or OCR. (not implemented)
+      3    Fully automatic page segmentation, but no OSD. (Default)
+      4    Assume a single column of text of variable sizes.
+      5    Assume a single uniform block of vertically aligned text.
+      6    Assume a single uniform block of text.
+      7    Treat the sourceImage as a single text line.
+      8    Treat the sourceImage as a single word.
+      9    Treat the sourceImage as a single word in a circle.
+     10    Treat the sourceImage as a single character.
+     11    Sparse text. Find as much text as possible in no particular order.
+     12    Sparse text with OSD.
+     13    Raw line. Treat the sourceImage as a single text line,
+           bypassing hacks that are Tesseract-specific.
 
-    articles = createArticles(page1)
+Other Features:
+    c      Draw Careas limits and show sourceImage
+    l      Draw Lines limits and show sourceImage
+    i      Draw Images limits and show sourceImage
+    a      Draw Articles limits and show sourceImage
+    e      Extract Images into out/
+
+Usage: python3 main.py relativePathToSourceImage relativePathToOutputFile PSM otherFeatures
+Example: python3 main.py assets/assistenteAoEmigrante_0.png out/aoe0.hocr 3 cie\n""")
+
+
+def main():
+    # Print help menu
+    if len(sys.argv) == 1 or "help" in sys.argv:
+        help()
+        quit()
+
+    if len(sys.argv) >= 4:
+        # Open sourceImage from argv[1]
+        sourceImage = PIL.Image.open(sys.argv[1])
+
+        # Create HOCR output in argv[2]
+        hocr = pytesseract.image_to_pdf_or_hocr(sourceImage,
+                                                config='--psm ' + sys.argv[3],
+                                                lang="por", extension='hocr')
+        with open(sys.argv[2], 'w+b') as f:
+            f.write(hocr)
+
+        page1 = parseHocr()
+
+    articles = copy.deepcopy(page1.careas)
     # articles = organizeArticles(articles)
     createMarkdown(articles)
 
-    # drawCareaBoxes(image, page1)
-    # drawParBoxes(image, page1)
-    # drawLinesBoxes(image, page1)
-    # drawPhotosBoxes(image, page1)
-    drawArticlesBoxes(image, articles)
+    if len(sys.argv) == 5:
+        if "c" in sys.argv[4]:
+            drawCareaBoxes(sourceImage, page1)
+        if "p" in sys.argv[4]:
+            drawParBoxes(sourceImage, page1)
+        if "l" in sys.argv[4]:
+            drawLinesBoxes(sourceImage, page1)
+        if "i" in sys.argv[4]:
+            drawImageBoxes(sourceImage, page1)
+        if "a" in sys.argv[4]:
+            drawArticlesBoxes(sourceImage, articles)
+        if "e" in sys.argv[4]:
+            extractImages(sourceImage, page1)
 
-    image.show()
-
-    # extractPhotos(image, page1)
+        sourceImage.show()
 
 
 if __name__ == "__main__":
